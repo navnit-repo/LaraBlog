5429327cffcfe699f2e6ea214e597803e0667b6c
Nit Navodit <navodit.nit@gmail.com>
2021-05-22 02:24:32 +0530

    footer

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,27 +1,191 @@
-<footer class="container-fluid border-top">
-    <div class="row purple-text text-darken-4 footer">
-        <div class="col-sm-2 text-center">Proudly powered by
-            <a href="https://github.com/alimranahmed/LaraBlog" target="_blank">LaraBlog</a>
-        </div>
-        <div class="col-sm-2 text-center">© {{date('Y').' '.$globalConfigs->copyright_owner}} </div>
-        @if(!auth()->check())
-            <div class="col-sm-1 text-center">
-                <span class="pointer text-info" data-toggle="modal" data-target="#subscribe-form">Subscribe</span>
-            </div>
-            <div class="col-sm-1 text-center">
-                <span class="pointer text-info" data-toggle="modal" data-target="#feedback-form">Feedback</span>
-            </div>
-            <div class="col-sm-1 col-sm-offset-5 text-center">
-                <a href="{{route('login-form')}}" class="text-grey">Manage</a>
-            </div>
-        @endif
-    </div>
+<footer class="page_footer  ds ms ">
+                <div class="container mt-5 mb-5">
+                    <div class="row align-center ">
+                        <div class="col-xl-6 col-lg-5 text-center text-lg-left">
+                            <div
+                                class="widget-odd widget-last widget-first widget-1 widget-theme-wrapper widget_no_background">
+                                <div id="mwt_bloginfo-5" class="widget widget_bloginfo"> <a class="blog-info-logo"
+                                        href="./" target="_blank">
+                                        <img src="./Starsgyan_logo_footer.png" class="w-50" alt="Logo">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="divider-60 divider-lg-0"></div>
+                        </div>
+                        <div class="col-xl-2 col-lg-2 align-self-start text-center text-lg-left">
+                            <div
+                                class="widget-odd widget-last widget-first widget-1 special-menu widget-theme-wrapper widget_no_background">
+                                <div id="nav_menu-2" class="widget widget_nav_menu">
+                                    <h3 class="widget-title">Services</h3>
+                                    <div class="menu-footer_menu-container">
+                                        <ul id="menu-footer_menu" class="menu">
+                                            <li id="menu-item-5498"
+                                                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5498">
+                                                <a href="./shopping.html">Shop</a></li>
+                                            <li id="menu-item-5499"
+                                                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5499">
+                                                <a href="./services">Services</a></li>
+                                            <li id="menu-item-5500"
+                                                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5500">
+                                                <a href="./astrologersform.html">Join</a></li>
+                                            <li id="menu-item-5503"
+                                                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5503">
+                                                <a href="./contacts.html">Contact Us</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="divider-60 divider-lg-0"></div>
+                        </div>
+                        <div class="col-xl-2 col-lg-2 align-self-start text-center text-lg-left">
+                            <div
+                                class="widget-odd widget-last widget-first widget-1 special-menu widget-theme-wrapper widget_no_background">
+                                <div id="nav_menu-3" class="widget widget_nav_menu">
+                                    <h3 class="widget-title">About Us</h3>
+                                    <div class="menu-footer_menu2-container">
+                                        <ul id="menu-footer_menu2" class="menu">
+                                            <li id="menu-item-5501"
+                                                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5501">
+                                                <a href="./blog.html">Blog</a></li>
+                                            <li id="menu-item-5502"
+                                                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5502">
+                                                <a href="./about.html">About</a></li>
+                                            <li id="menu-item-5503"
+                                                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5503">
+                                                <a href="./tc.html" target="_blank">Terms & Conditions</a></li>
+                                            <li id="menu-item-5503"
+                                                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5503">
+                                                <a href="./tc.html" target="_blank">Return Refund & Cancellation Policy</a></li>
+                                            <li id="menu-item-5503"
+                                                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5503">
+                                                <a href="./policy.html" target="_blank">Privacy Policies</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="divider-60 divider-lg-0"></div>
+                        </div>
+                        <div class="col-xl-2 col-lg-3 align-self-start text-center text-lg-left">
+                            <div
+                                class="widget-odd widget-last widget-first widget-1 widget-theme-wrapper widget_no_background">
+                                <div id="mwt_bloginfo-4" class="widget widget_bloginfo">
+                                    <h3 class="widget-title">Contact Info</h3>
+                                    <div class="description">
+                                    	<a href="mailto:contact@starsgyan.com">contact@starsgyan.com</a>
+                                    	</div>
 
-    @include("layouts._modal_subscribe_form")
-    @include("layouts._modal_feedback_form")
-</footer>
-</div>
-<script src="{{ mix("build/js/app.js") }}" type="application/javascript"></script>
-@yield('inPageJS')
+                                    <span class="social-icons ">
+
+                                        <a href="https://www.facebook.com/AppStarsGyan/" target="_blank" class="fa fa-facebook  " style="color: white;">
+                                            <span class=""></span>
+                                        </a>
+
+                                        <a href="https://twitter.com/" target="_blank" class="fa fa-tumblr  " style="color: white;">
+                                            <span class=""></span>
+                                        </a>
+
+                                        <a href="https://www.instagram.com/starsgyanapp/" target="_blank" class="fa fa-instagram  " style="color: white;">
+                                            <span class=""></span>
+                                        </a>
+
+                                        <a href="https://www.linkedin.com/" target="_blank" class="fa fa-linkedin  " style="color: white;">
+                                            <span class=""></span>
+                                        </a>
+
+                                        <a href="https://youtube.com/channel/UCHyIXY2IoZ-ltRiUbByZG0A" target="_blank" class="fa fa-youtube-play  " style="color: white;">
+                                            <span class=""></span>
+                                        </a>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+            <!-- .page_footer -->
+
+			<!-- .page_footer -->
+			
+			<section class="page_copyright  ds ms s-pt-25 s-pb-25   s-bordertop-container          ">
+				<div class="container">
+					<div class="row align-items-center">
+						<div class="col-sm-12 text-center">
+							© Copyright 2020 All Rights Reserved </div>
+					</div>
+				</div>
+			</section><!-- .page_copyright -->
+		</div><!-- eof #box_wrapper -->
+	</div>
 </body>
+<style>
+
+    .astext{
+    background:none;
+    border:none;
+    margin:0;
+    padding:0;
+    cursor: pointer;
+     }
+     .astext:hover{
+     	color:#851f37;
+     }
+    .s-overlay.ds:before {
+        background-color: white;
+        color: black;
+        opacity: 0.9;
+    }
+
+    .btn-outline-maincolor {
+        border-color: #ca6730;
+    }
+
+    .ds a:not(.btn) {
+        color: black;
+    }
+
+    .ds .btn-outline-maincolor,
+    .ds .btn-outline-maincolor2,
+    .ds .btn-outline-maincolor3,
+    .ds .btn-outline-darkgrey {
+        color: #ca6730;
+    }
+
+    /*.s-overlay.ds.ms:before {*/
+    /*    background-color: #851f37;*/
+    /*}*/
+.s-overlay.ds.ms:before {
+    background-color: #EF7F1A;
+}
+    /*@media (min-width: 1200px) {
+
+        .ds .sf-menu>li>a:hover,
+        .ds .sf-menu>li.active>a {
+            color: white;
+        }
+    }*/
+
+    .ds a:not(.btn) {
+        color: white;
+    }
+
+    
+
+    .ico-right-arrow:before {
+        content: "\e90e";
+        color: #851f37;
+    }
+
+
+    .ds .btn-outline-maincolor,
+    .ds .btn-outline-maincolor2,
+    .ds .btn-outline-maincolor3,
+    .ds .btn-outline-darkgrey {
+        color: white;
+        background: #ca6730;
+	}
+	.ds.bs {
+		background-color: black;
+	}
+	
+</style>
 </html>
